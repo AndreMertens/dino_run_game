058d48023961a2634901dfc0e3012ae1ba6c18d4
Andre Mertens <andre.mertens@inform-software.com>
2024-01-19 14:08:42 +0100

    feat(background): create moving background

--- lib/game/dino_run.dart
+++ lib/game/dino_run.dart
@@ -1,1 +1,87 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame/parallax.dart';
 
+// This is the main flame game class.
+class DinoRun extends FlameGame with TapDetector, HasCollisionDetection {
+  DinoRun({super.camera});
+
+  // List of all the image assets.
+  static const _imageAssets = [
+    'DinoSprites - tard.png',
+    'AngryPig/Walk (36x30).png',
+    'Bat/Flying (46x30).png',
+    'Rino/Run (52x34).png',
+    'parallax/plx-1.png',
+    'parallax/plx-2.png',
+    'parallax/plx-3.png',
+    'parallax/plx-4.png',
+    'parallax/plx-5.png',
+    'parallax/plx-6.png',
+  ];
+
+  // 60.
+
+  // 9.
+
+  // 20.
+
+  // 38.
+
+  // 85.
+
+  Vector2 get virtualSize => camera.viewport.virtualSize;
+
+  // This method get called while flame is preparing this game.
+  @override
+  Future<void> onLoad() async {
+    // Makes the game full screen and landscape only.
+    await Flame.device.fullScreen();
+    await Flame.device.setLandscape();
+
+    // 39.
+
+    // 86.
+
+    // 61.
+
+    // Cache all the images.
+    await images.loadAll(_imageAssets);
+
+    // This makes the camera look at the center of the viewport.
+    camera.viewfinder.position = camera.viewport.virtualSize * 0.5;
+
+    /// Create a [ParallaxComponent] and add it to game.
+    final parallaxBackground = await loadParallaxComponent(
+      [
+        ParallaxImageData('parallax/plx-1.png'),
+        ParallaxImageData('parallax/plx-2.png'),
+        ParallaxImageData('parallax/plx-3.png'),
+        ParallaxImageData('parallax/plx-4.png'),
+        ParallaxImageData('parallax/plx-5.png'),
+        ParallaxImageData('parallax/plx-6.png'),
+      ],
+      baseVelocity: Vector2(10, 0),
+      velocityMultiplierDelta: Vector2(1.4, 0),
+    );
+
+    // Add the parallax as the backdrop.
+    camera.backdrop.add(parallaxBackground);
+
+    // 10.
+
+    // 40.
+  }
+
+  // 11.
+
+  // 16.
+
+  // 41.
+
+  // 45.
+
+  // 88.
+}
