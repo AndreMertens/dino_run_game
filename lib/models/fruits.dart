030e9e5ca66c1bdf5a0b6f70dd1e6bfd42da7828
noelhartmann <noel.hartmann@inform-software.com>
2025-01-28 16:38:20 +0100

    add tiled

---
+++ lib/models/fruits.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:dino_run_game/game/dino_run.dart';
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+
+import 'custom_hitbox.dart';
+
+class Fruit extends SpriteAnimationComponent
+    with HasGameRef<DinoRun>, CollisionCallbacks {
+  final String fruit;
+  Fruit({
+    this.fruit = 'Apple',
+    position,
+    size,
+  }) : super(
+          position: position,
+          size: size,
+        );
+
+  final double stepTime = 0.05;
+  final hitbox = CustomHitbox(
+    offsetX: 10,
+    offsetY: 10,
+    width: 12,
+    height: 12,
+  );
+  bool collected = false;
+
+  @override
+  FutureOr<void> onLoad() {
+    priority = -1;
+
+    add(
+      RectangleHitbox(
+        position: Vector2(hitbox.offsetX, hitbox.offsetY),
+        size: Vector2(hitbox.width, hitbox.height),
+        collisionType: CollisionType.passive,
+      ),
+    );
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('Items/Fruits/$fruit.png'),
+      SpriteAnimationData.sequenced(
+        amount: 17,
+        stepTime: stepTime,
+        textureSize: Vector2.all(32),
+      ),
+    );
+    return super.onLoad();
+  }
+
+  void collidedWithPlayer() async {
+    if (!collected) {
+      collected = true;
+      game.playerData.currentScore += 1;
+      // if (game.playSounds) {
+      //   FlameAudio.play('collect_fruit.wav', volume: game.soundVolume);
+    }
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('Items/Fruits/Collected.png'),
+      SpriteAnimationData.sequenced(
+        amount: 6,
+        stepTime: stepTime,
+        textureSize: Vector2.all(32),
+        loop: false,
+      ),
+    );
+
+    await animationTicker?.completed;
+    removeFromParent();
+  }
+}
